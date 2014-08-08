7c62c576da37259d2b2d732a65a1360e27fbdffd
thomasscarroll89 <thomasscarroll89>
2014-08-08 16:16:19 -0500

    Tenth Update

--- day5/lab5.py
+++ day5/lab5.py
@@ -30,7 +30,32 @@ print sum([len(ring[node]) for node in ring.keys()])/2
 # TODO: create a square graph with 256 nodes and count the edges 
 # TODO: define a function countEdges
 
+square = {}
+n = 16
+for i in range(1, (n**2)+1):
+	if (i%n)!=0:
+		square = makeLink(square, i, i+1)
+	if ((i+n)%(n))!=1:
+		square = makeLink(square, i, i-1)
+	if (i not in range(n**2 - n, n**2+1)):
+		square = makeLink(square, i, i+n)
+	if (i not in range(1, n+1)): 
+		square = makeLink(square, i, i-n)
 
+#print square
+	
+def countEdges(dictionary):
+	output = []
+	for i in dictionary.keys():
+		connected_nodes = dictionary[i].keys()
+		for j in connected_nodes:
+			if ((i, j) not in output):
+				output.append((i, j))
+				output.append((j, i))
+	return (len(output)/2)
+
+#print countEdges(square)
+	
 # Social Network
 class Actor(object):
   def __init__(self, name):
@@ -61,7 +86,12 @@ makeLink(movies, ah, ms) # Devil Wears Prada
 makeLink(movies, ah, jr) # Valentine's Day
 
 # How many nodes in movies?
+def countNodes(dictionary):
+	return len(dictionary.keys())
+
+print countNodes(movies)
 # How many edges in movies?
+print countEdges(movies)
 
 def tour(graph, nodes):
   for i in range(len(nodes)):
@@ -81,7 +111,7 @@ def tour(graph, nodes):
           break 
 
 # TODO: find an Eulerian tour of the movie network and check it 
-movie_tour = [] 
+movie_tour = [ms, rd, dh, ss, jr, dh, kb, ms, ah, jr, kb] 
 tour(movies, movie_tour)
 
 
@@ -98,10 +128,30 @@ def findPath(graph, start, end, path=[]):
         return None
 
 print findPath(movies, jr, ms)
-
+print type(findPath(movies, ms, ss))
 
 # TODO: implement findShortestPath()
-# print findShortestPath(movies, ms, ss)
+def findShortestPath(graph, start, end, path=[]):
+	if path == []:
+		initial_path = findPath(graph=graph, start=start, end=end, path=[])
+		length_initial_path = len(initial_path)
+	new_path = [start]
+	# if type(start)=="Actor":
+		# potential_moves = graph[start] #we want potential moves to be a function of whichever node we are currently on, which is indexed by the last element of start
+	# else:
+	potential_moves = graph[start[-1]]
+
+	if (end in potential_moves and (length_initial_path > (len(new_path) + 1))): #IF we can move to the end node in the next move
+		new_path.append(end)
+		return new_path
+	for next_move in potential_moves:
+		new_path_temp = new_path.append(next_move)
+		if len(new_path_temp) >= length_initial_path:
+			break
+		else:
+			findShortestPath(graph=graph, start=new_path_temp, end=end, path=new_path_temp)
+
+print findShortestPath(movies, ms, ss)
 
 # TODO: implement findAllPaths() to find all paths between two nodes
 # allPaths = findAllPaths(movies, jr, ms)
